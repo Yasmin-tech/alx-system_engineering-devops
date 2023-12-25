64cbb7672ef11a5ce736b76d881b9bd7d1775705
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-12-25 15:06:45 +0300

    add a comment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,12 @@
+# Using Puppet, create a file in /tmp.
+
+#Requirements:
+
+# File path is /tmp/school
+# File permission is 0744
+# File owner is www-data
+# File group is www-data
+# File contains I love Puppet
 file {
     '/tmp/school':
     owner   => www-data,
