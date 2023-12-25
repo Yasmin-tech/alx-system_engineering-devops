7b838b11e9f5cd8e10dc3ffc8a9be0c4c051df0d
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-12-25 16:30:12 +0300

    fix some code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,27 +6,26 @@
 # Version must be 2.1.0
 
 package {
-    'python3':
+    'python3.8':
     ensure  => '3.8.10',
 }
 
 package {
     'python3-pip':
     ensure  => installed,
-    require => Package['python3'],
 }
 
 package {
     'flask':
     ensure   => '2.1.0',
-    require  => Package['python3', 'python3-pip'],
-    provider => 'pip3',
+    require  => Package['python3-pip'],
+    provider => 'pip',
 }
 
 package {
     'Werkzeug':
     ensure   => '2.1.0',
-    require  => Package['python3', 'python3-pip', 'flask'],
-    provider => 'pip3',
+    require  => Package['python3-pip'],
+    provider => 'pip',
 }
 
