502e40e2a512a0c492189179e313639b2af10f75
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-12-25 15:56:45 +0300

    add task  1

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,32 @@
+# Using Puppet, install flask from pip3.
+
+# Requirements:
+
+# Install flask
+# Version must be 2.1.0
+
+package {
+    'python3':
+    ensure  => installed,
+}
+
+package {
+    'python3-pip':
+    ensure  => installed,
+    require => Package['python3'],
+}
+
+package {
+    'flask':
+    ensure   => '2.1.0',
+    require  => Package['python3', 'python3-pip'],
+    provider => 'pip3',
+}
+
+package {
+    'Werkzeug':
+    ensure   => '2.1.0',
+    require  => Package['python3', 'python3-pip'],
+    provider => 'pip3',
+}
+
