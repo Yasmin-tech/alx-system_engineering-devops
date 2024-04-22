e4f85c3585c8908bf191232ad0922821a37a3d7e
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-04-23 01:35:33 +0300

    test

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,51 +1,29 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
-import requests
-import sys
+# -*- coding: utf-8 -*-
+"""
+ Gather data from an API
+"""
+from requests import get
+from sys import argv
 
 
-def employee_tasks_info(EMPLOYEE_ID):
-    """request an employee data and task information from
+if __name__ == '__main__':
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    response = get(url)
+    name = response.json().get('name')
 
-    https://jsonplaceholder.typicode.com/
-    REST API
-    """
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+    response = get(url)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
 
-    # response1 to get the employee name
-    # response2 to get all the todos
-    response1 = requests.get(
-            "https://jsonplaceholder.typicode.com/users/{}".format(EMPLOYEE_ID)
-            )
-    response2 = requests.get(
-            "https://jsonplaceholder.typicode.com/todos".format(EMPLOYEE_ID)
-            )
-
-    employee = response1.json()
-    EMPLOYEE_NAME = employee.get("name")
-
-    response2_dict = response2.json()
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
-    list_done_tasks = []
-
-    for task in response2_dict:
-        user_id = task.get("userId", -1)
-        if user_id == int(EMPLOYEE_ID):
-            if task.get("completed") is True:
-                NUMBER_OF_DONE_TASKS += 1
-                TOTAL_NUMBER_OF_TASKS += 1
-                list_done_tasks.append(task.get("title"))
-            else:
-                TOTAL_NUMBER_OF_TASKS += 1
-
-    print(
-        f'Employee {EMPLOYEE_NAME} is done with tasks'
-        f'({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):'
-    )
-    for task in list_done_tasks:
-        print("\t", task)
-
-
-if __name__ == "__main__":
-    EMPLOYEE_ID = sys.argv[1]
-    employee_tasks_info(EMPLOYEE_ID)
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, done, len(tasks)))
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
