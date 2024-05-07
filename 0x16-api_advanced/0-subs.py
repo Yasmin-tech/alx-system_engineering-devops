2dfd67b43719379bba71ea1b8ddfbafa03ed2f5e
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-05-07 15:45:01 +0300

    add task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A module that retrieves the number of subscribers of a subreddit."""
+
+
+import requests
+# import pprint
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers (total subscribers)
+        for a given subreddit. If an invalid subreddit is given,
+        the function will return 0"""
+
+    headers = {"User-Agent": "My User Agent 1.0"}
+    response = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/about.json", headers=headers
+    )
+    if response.status_code == 200:
+        return response.json()["data"]["subscribers"]
+    else:
+        return 0
+
+
+# pprint.pprint(get_subreddit_subscribers("programming"))
