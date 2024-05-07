698c8390fd54fdec66c0226a4cd1c98d9fe55714
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-05-08 00:17:31 +0300

    add task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,19 +4,26 @@
 import requests
 
 
-def top_ten(subreddit):
+def recurse(subreddit, hot_list=[], after=""):
     """a function that queries the Reddit API and prints the titles of the
     first 10 hot posts listed for a given subreddit."""
 
     headers = {"User-Agent": "Middle-Chipmunk-3601"}
+    param = {"after": after}
     response = requests.get(
         "https://www.reddit.com/r/{}/hot.json".format(subreddit),
         headers=headers,
-        allow_redirects=False,
+        allow_redirects=False, params=param
     )
     if response.status_code == 200:
         result = response.json()
-        for i in range(10):
-            print(result['data']['children'][i]['data']['title'])
+        list_chlidren = result['data']['children']
+        list_titles = [i['data']['title'] for i in list_chlidren]
+        # print(list_titles)
+        hot_list.extend(list_titles)
+        if result['data']['after']:
+            return recurse(subreddit, hot_list, after=result['data']['after'])
+        else:
+            return hot_list
     else:
         print(None)
