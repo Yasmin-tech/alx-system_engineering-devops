acdcacb645bc39e75484d639000f2ea6d1700461
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-05-07 23:18:50 +0300

    add task 00

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
-import requests
+"""A module that retrieves the number of subscribers of a subreddit."""
 
-CLIENT_ID = "giJNSgUJI9j600G1max3iA"
-SECRET_KEY = "DDF9lor7MbKzT5TgqvRYqjHapJC66w"
-auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
+
+import requests
+# import pprint
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    data = {
-        'grant_type': 'password',
-        'username': 'Middle-Chipmunk-3601',
-        'password': '123456poiuyt'
-    }
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1 (by /u/Middle-Chipmunk-3601)"
-    }
-    response = requests.post('https://www.reddit.com/api/v1/access_token',
-                             data=data, headers=headers, auth=auth)
-    TOKEN = response.json().get('access_token')
-    headers['Authorization'] = f"bearer {TOKEN}"
-    # response2 = requests.get('https://oauth.reddit.com/api/v1/me',
-    #                          headers=headers)
-    # return response2.json()
+    """Returns the number of subscribers (total subscribers)
+        for a given subreddit. If an invalid subreddit is given,
+        the function will return 0"""
 
-    url = "https://oauth.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My User Agent 1.0"}
+    response = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers=headers,
+        allow_redirects=False
+    )
+    if response.status_code == 200:
+        result = response.json().get("data")
+        return result.get("subscribers")
+    else:
+        return 0
 
-    response3 = requests.get(url, headers=headers, allow_redirects=False)
-    if response3.status_code == 200:
-        return response3.json().get("data").get("subscribers")
-    return 0
+
+# pprint.pprint(get_subreddit_subscribers("programming"))
