018b376380f55505cadbb45ae720e89d7afad0e0
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-05-07 23:05:32 +0300

    task 0 again

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,20 +2,32 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-CLIENT_ID = "JuROsJFzp0mSoul6h-hA_g"
-SECRET_KEY = "SqvfdwgArZ5yzy0oPE_kA1qqsBEknA"
+CLIENT_ID = "giJNSgUJI9j600G1max3iA"
+SECRET_KEY = "DDF9lor7MbKzT5TgqvRYqjHapJC66w"
 auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    data = {
+        'grant_type': 'password',
+        'username': 'Middle-Chipmunk-3601',
+        'password': '123456poiuyt'
     }
-    response = requests.get(url, headers=headers, allow_redirects=False,
-                            auth=auth)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1 (by /u/Middle-Chipmunk-3601)"
+    }
+    response = requests.post('https://www.reddit.com/api/v1/access_token',
+                             data=data, headers=headers, auth=auth)
+    TOKEN = response.json().get('access_token')
+    headers['Authorization'] = f"bearer {TOKEN}"
+    # response2 = requests.get('https://oauth.reddit.com/api/v1/me',
+    #                          headers=headers)
+    # return response2.json()
+
+    url = "https://oauth.reddit.com/r/{}/about.json".format(subreddit)
+
+    response3 = requests.get(url, headers=headers, allow_redirects=False)
+    if response3.status_code == 200:
+        return response3.json().get("data").get("subscribers")
+    return 0
