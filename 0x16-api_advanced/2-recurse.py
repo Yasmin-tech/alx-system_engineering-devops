8a05e495ff7dcca7cec4505ee9191493cb08afe6
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-05-08 00:33:33 +0300

    task 2 again

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,8 +5,10 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=""):
-    """a function that queries the Reddit API and prints the titles of the
-    first 10 hot posts listed for a given subreddit."""
+    """A function that queries the Reddit API and returns the titles of
+    all hot posts listed for a given subreddit."""
+    if not hot_list:
+        hot_list = []
 
     headers = {"User-Agent": "Middle-Chipmunk-3601"}
     param = {"after": after}
@@ -15,16 +17,17 @@ def recurse(subreddit, hot_list=[], after=""):
         headers=headers,
         allow_redirects=False, params=param
     )
-    if response.status_code == 200:
-        result = response.json()
-        list_chlidren = result['data']['children']
-        list_titles = [i['data']['title'] for i in list_chlidren]
-        # print(list_titles)
-        hot_list.extend(list_titles)
-        if result['data']['after']:
-            return recurse(subreddit, hot_list=hot_list,
-                           after=result['data']['after'])
-        else:
-            return hot_list
+
+    if response.status_code != 200:
+        return None
+
+    result = response.json()
+    list_children = result['data']['children']
+    list_titles = [i['data']['title'] for i in list_children]
+    hot_list.extend(list_titles)
+
+    if result['data']['after']:
+        return recurse(subreddit, hot_list=hot_list,
+                       after=result['data']['after'])
     else:
-        print(None)
+        return hot_list
